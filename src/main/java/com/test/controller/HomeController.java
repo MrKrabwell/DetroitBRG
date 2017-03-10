db56c956ddb6cd0ccb1bdc04407bc923d15f0e56
MrKrabwell <yosuke.kanii@gmail.com>
2017-03-10 16:21:23 -0500

    Initial commit, index page is only thing that is working/shown

---
+++ src/main/java/com/test/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public ModelAndView showIndexPage()
+    {
+        return new
+                ModelAndView("index","message","Go Team 3!!");
+
+    }
+
+}
